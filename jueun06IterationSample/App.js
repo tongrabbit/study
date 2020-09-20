65bfc1ca74071e488d08a5f10a1933e8f85ac6df
tongrabbit <71494862+tongrabbit@users.noreply.github.com>
2020-09-20 16:56:49 +0900

    Create App.js

---
+++ jueun06IterationSample/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import IterationSample from './IterationSample';
+
+class App extends Component {
+  render() {
+    return (
+      <IterationSample/>
+    );
+  }
+}
+
+export default App;
