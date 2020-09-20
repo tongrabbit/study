72e640a7211724bef4ef5fef22cefc68167b504a
tongrabbit <71494862+tongrabbit@users.noreply.github.com>
2020-09-20 16:56:31 +0900

    Create IteraionSample.js

---
+++ jueun06IterationSample/IteraionSample.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+
+class IterationSample extends Component{
+	state={
+		names : ['눈사람','얼음','눈','바람'],
+		name:''
+	};
+	
+	handleChange = (e) =>{
+		this.setState({
+			name:e.target.value
+		});
+	}
+	handleInsert = () =>{
+		this.setState({
+			names:this.state.names.concat(this.state.name),
+			name:''
+		});
+	}
+	
+	handleRemove = (index) =>{
+		const {names} = this.state;
+		
+		this.setState({
+			names:[
+				//...은 현재 배열을 복사
+				...names.slice(0,index),
+				...names.slice(index+1,names.length)
+			]
+		});
+	}
+	render(){
+		const nameList = this.state.names.map(
+			(name,index) => (<li key={index} onDoubleClick={()=>this.handleRemove(index)}>{name}</li>)
+		);
+		
+		return(
+			<div>
+				<input onChange={this.handleChange} value={this.state.name}/>
+				<button onClick={this.handleInsert}>추가</button>
+				<ul>
+					{nameList}
+				</ul>
+			</div>
+		);
+	}
+}
+
+export default IterationSample;
